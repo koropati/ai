cc3e32b43242950ed7d442e5b42bf31d17b09837
dewasatriawan <dewa@bsa.id>
2024-11-01 09:35:40 +0800

    update example electre

--- example/electre.py
+++ example/electre.py
@@ -4,12 +4,19 @@ import numpy as np
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src import ELECTRE
+from utils import ExcelReader
 
-# Contoh penggunaan
-alternatives = [0, 1, 2]  # Kode alternatif
-criteria = np.array([[2, 3, 3, 4, 3, 4, 5], [3, 3, 2, 4, 5, 2, 3], [4, 3, 3, 2, 2, 4, 2]])  # Matriks kriteria
-weights = [0.2, 0.15, 0.2, 0.1, 0.15, 0.1, 0.1]  # Bobot kriteria
-alternative_labels = {0: "A1", 1: "A2", 2: "A3"}
+filepath = "data/saw-thk-aneka.xlsx"
+
+# Membaca data dari Excel
+excel_reader = ExcelReader(filepath)
+data = excel_reader.read_excel()
+
+alternatives = data['alternatives']
+criteria = data['criteria_values']
+weights = data['weights']
+# criteria_type = data['criteria_type']
+alternative_labels = data['alternative_labels']
 
 electre = ELECTRE(alternatives, criteria, weights, alternative_labels)
 electre.normalize()
